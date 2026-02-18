89fd4fd0420b4e7d66a5e9c840d80a943d4a545c
maheshray1221 <mr5832388@gmail.com>
2026-02-18 12:55:00 +0530

    initialize setup complete

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+
+
+connectDB()
+    .then(() => {
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => {
+            console.log(`Server is running in port ${port}`);
+        })
+    })
+    .catch((err) => {
+        console.log("mongo db connection failed", err);
+    })
